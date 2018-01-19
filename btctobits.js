a32661f81cd13aacff117760b607990c5f6f74d2
Colin Kierans <kierans@monstercat.com>
2018-01-19 13:14:57 -0800

    Took care of more edge cases.

--- btctobits.js
+++ btctobits.js
@@ -4,7 +4,7 @@
   return a;
 }
 
-var reg = new RegExp(/([0-9,]?\.?[0-9,]+?)\s?btc/gi);
+var reg = new RegExp(/([0-9,]?\.?[0-9,]+?)\s?(btc|bitcoins|bitcoin)/gi);
 var texts = textNodesUnder(document.body);
 var totalReplaced = 0;
 texts.forEach(function (node) {
@@ -15,15 +15,27 @@ texts.forEach(function (node) {
     var btcPlaceholder = {};
     do {
       match = reg.exec(node.innerText);
-      if(match != null) {
+      if(match != null && match[1] != ',') {
         hasMatches = true;
         var btcPrice = match[0];
 
-        var num = new Number(match[1].split(',').join(''));
+        var numparts = match[1].split(',');
+        var numstr = "";
+
+        //Here I am trying to detect someone writing "12,5" and meaning 12.5
+        if(numparts.length == 2 && numparts[1].toString().length < 3) {
+          numstr = numparts.join('.');
+        }
+        else {
+          numstr = numparts.join(',');
+        }
+
+        var num = new Number(numstr);
         var placehodlerId = Math.random() + '_' + new Date().getTime();
         btcPlaceholder[placehodlerId] = btcPrice;
-        var bits = (num * 1000000).toLocaleString() + ' bits (' + placehodlerId + ')';
-        newText = newText.replace(match[0], bits);
+        var bits = (num * 1000000).toLocaleString();
+        var replacement = bits + ' bits (' + placehodlerId + ')';
+        newText = newText.replace(match[0], replacement);
         totalReplaced++
       }
     }
